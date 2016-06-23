d9a4ff9cf20b1a6c8cf1bc78093300ec083d5d3a
bingoogolapple <bingoogolapple@gmail.com>
2016-06-23 23:28:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,21 @@
+/**
+ * Created by bingoogolapple on 16/6/23.
+ */
+var express = require("express");
+var app = express();
+var router = require("./router/router.js");
+
+
+// 设置静态资源目录
+app.use(express.static("./public"));
+
+// 获取评价列表
+app.get("/comment", router.getComments);
+
+// 添加评论
+app.post("/comment", router.addComment);
+
+//404
+app.use(router.notFound);
+
+app.listen(3000);
